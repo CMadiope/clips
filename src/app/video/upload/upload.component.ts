8e44c3eb13a86f1cc167eb3238a9ec516437d39c
Confidence Madiope <confidencem97@gmail.com>
2023-07-03 11:11:14 +0200

    .

---
+++ src/app/video/upload/upload.component.ts
@@ -0,0 +1,84 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { v4 as uuid } from 'uuid';
+import { last } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-upload',
+  templateUrl: './upload.component.html',
+  styleUrls: ['./upload.component.css'],
+})
+export class UploadComponent {
+  isDragover = false;
+  file: File | null = null;
+  nextStep = false;
+  showAlert = false;
+  alertColor = 'blue';
+  alertMsg = 'Please wait! Your clip is being uploaded.';
+  inSubmission = false;
+  percentage = 0;
+  showPercentage = false;
+
+  title = new FormControl('', {
+    validators: [Validators.required, Validators.minLength(3)],
+    nonNullable: true,
+  });
+
+  uploadForm = new FormGroup({
+    title: this.title,
+  });
+  constructor(private storage: AngularFireStorage) {}
+
+  storeFile($event: Event) {
+    this.isDragover = false;
+
+    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null;
+
+    if (!this.file || this.file.type !== 'video/mp4') {
+      return;
+    }
+    this.title.setValue(this.file.name.replace(/\.[^/.]+$/, ''));
+
+    this.nextStep = true;
+  }
+
+  uploadFile() {
+    this.showAlert = true;
+    this.alertColor = 'blue';
+    this.alertMsg = 'Please wait! Your clip is being uploaded.';
+    this.inSubmission = true;
+    this.showPercentage = true;
+
+    const clipFileName = uuid();
+    const clipPath = `clips/${clipFileName}.mp4`;
+
+    const task = this.storage.upload(clipPath, this.file);
+
+    task.percentageChanges().subscribe((progress) => {
+      this.percentage = (progress as number) / 100;
+    });
+
+    task
+      .snapshotChanges()
+      .pipe(last())
+      .subscribe({
+        next: (snapshot) => {
+          this.alertColor = 'green';
+          this.alertMsg =
+            'Success! Your clip is now ready to share with the world';
+          this.showPercentage = false;
+        },
+        error: (error) => {
+          this.alertColor = 'red';
+          this.alertMsg = 'Upload failed! Please try again later';
+          this.inSubmission = true;
+          this.showPercentage = false;
+          console.log(error);
+        },
+      });
+  }
+}
+
+//Confidence@1883664cc
+//madiope@gmail.com
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBmB6v2PRF5yeDI86CB3jsV3zyCD4r-Ywk',
+    authDomain: 'clips-7469b.firebaseapp.com',
+    projectId: 'clips-7469b',
+    storageBucket: 'clips-7469b.appspot.com',
+    messagingSenderId: '775776825492',
+    appId: '1:775776825492:web:0c49a6fcc8d232d29f4481',
+  },
+};
